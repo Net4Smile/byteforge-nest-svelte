ab71f93f279b57fecce08b9c4ffcd7631b4dacc9
Net4Smile <mikolaj.wator@o2.pl>
2024-11-30 15:09:10 +0100

    simple product fetch to frontend app through apollo

--- frontend/src/lib/client.ts
+++ frontend/src/lib/client.ts
@@ -1,7 +1,22 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client/core';
-import type { NormalizedCacheObject } from '@apollo/client/core';
+import { HttpLink } from '@apollo/client/link/http';
+import { browser } from '$app/environment';
 
-export const client = new ApolloClient<NormalizedCacheObject>({
-  uri: '/api/graphql',
-  cache: new InMemoryCache()
-});
+let client: ApolloClient<any>;
+
+export function getClient(fetch?: (input: RequestInfo, init?: RequestInit) => Promise<Response>) {
+  if (client && browser) return client;
+
+  const httpLink = new HttpLink({
+    uri: '/api/graphql',
+    fetch: fetch || (browser ? window.fetch.bind(window) : undefined) as any,
+  });
+
+  client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache(),
+    ssrMode: !browser,
+  });
+
+  return client;
+}
---
+++ frontend/src/lib/handlers/products.ts
@@ -0,0 +1,57 @@
+import { getClient } from '$lib/client';
+import { gql, type ApolloQueryResult, type TypedDocumentNode } from '@apollo/client/core';
+import type { Query } from '../../generated/graphql';
+
+export async function getProduct(id: string, fetch?: (input: RequestInfo, init?: RequestInit) => Promise<Response>, client = getClient(fetch)): Promise<ApolloQueryResult<Pick<Query, "getProduct">>> {
+  const PRODUCT_QUERY: TypedDocumentNode<Query["getProduct"], { id: string }> = gql(`
+    query Product($id: String!) {
+      getProduct(id: $id) {
+        description
+        id
+        image {
+          src
+          alt
+        }
+        name
+        price
+      }
+    }
+  `)
+
+  const request = await client.query<Pick<Query, "getProduct">>({
+    query: PRODUCT_QUERY,
+    variables: {
+      id
+    }
+  });
+
+  return request;
+}
+
+export async function getProducts(fetch?: (input: RequestInfo, init?: RequestInit) => Promise<Response>): Promise<ApolloQueryResult<Pick<Query, "getProducts">>> {
+  const client = getClient(fetch);
+  const PRODUCTS_QUERY: TypedDocumentNode<Pick<Query, "getProducts">, {}> = gql`
+    query Products {
+      getProducts {
+        id
+        name
+        description
+        price
+        image {
+          src
+          alt
+        }
+      }
+    }
+  `;
+
+  try {
+    const result = await client.query<Pick<Query, "getProducts">>({
+      query: PRODUCTS_QUERY
+    });
+    return result;
+  } catch (error) {
+    console.error('Error fetching products:', error);
+    throw error;
+  }
+}
