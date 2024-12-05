665ec55637fa2a32bb330a9a748aac1510b7e304
Net4Smile <mikolaj.wator@o2.pl>
2024-12-05 14:16:39 +0100

    better main layout structure

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,23 +1,33 @@
 <script lang="ts">
-	import Navigation from "$lib/components/Navigation.svelte";
-	  import { isMobile } from "$lib/stores/shared.svelte";
-	import { onMount } from "svelte";
+	import Navigation from "$lib/components/navigation";
+	import { isMobile } from "$lib/stores/shared.svelte";
+	import Aside from "$lib/components/aside";
 	import "../app.css";
+	import "$lib/stringExtensions"
 	
-	  let { children } = $props();
+  let { children } = $props();
 	
-	  onMount(() => {
-	      $effect(() => {
-	          const mediaQuery = window.matchMedia("(max-width: 768px)");
-	          const checkMobile = () => ($isMobile = mediaQuery.matches);
-	  
-	          checkMobile();
-	          mediaQuery.addEventListener("change", checkMobile);
-	  
-	          return () => mediaQuery.removeEventListener("change", checkMobile);
-	      });
-	  })
+  $effect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 767px)");
+    const checkMobile = () => ($isMobile = mediaQuery.matches);
+
+    checkMobile();
+    mediaQuery.addEventListener("change", checkMobile);
+
+    return () => mediaQuery.removeEventListener("change", checkMobile);
+  });
 </script>
 
 <Navigation />
-{@render children()}
+<main class="wrapper">
+  <Aside />
+	<section class="w-full flex items-center justify-center gap-2">
+		{@render children()}
+	</section>
+</main>
+
+<style lang="postcss">
+  .wrapper {
+    @apply flex flex-col md:flex-row gap-2 w-full max-w-screen-2xl mx-auto my-3;
+  }
+</style>
